901c09b8e5e2e07ca190661ae5a00697fdd7706e
Ahmed <Ahmed.Khaled01144271924@gmail.com>
2024-02-04 20:37:51 +0200

    update slide details

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,10 +1,14 @@
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+
+var swiper = new Swiper('.swiper-container', {
+  // Swiper options and settings
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
